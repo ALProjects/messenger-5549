dfd7a68872192affd0c6deaefe15a1ee4c861990
Aaron Li <aaronhf.li@mail.utoronto.ca>
2021-08-26 10:53:48 -0400

    Initial dev environment setup

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,15 @@
 const Sequelize = require("sequelize");
 
+/*
 const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
 });
+*/
 
-module.exports = db;
+const sequelize = new Sequelize('messenger', 'aaron', 'psqlpass', {
+  host: 'localhost',
+  dialect: 'postgres',
+  logging: false
+});
+
+module.exports = sequelize;
