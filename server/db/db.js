bd9a7ead3b77659a622dee375d54730a4e77dbb0
Aaron Li <aaronhf.li@mail.utoronto.ca>
2021-08-31 12:04:41 -0400

    Reimplement connection as per code review comment

--- server/db/db.js
+++ server/db/db.js
@@ -1,15 +1,8 @@
+require('dotenv').config();
 const Sequelize = require("sequelize");
 
-/*
 const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
 });
-*/
 
-const sequelize = new Sequelize('messenger', 'aaron', 'psqlpass', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false
-});
-
-module.exports = sequelize;
+module.exports = db;
